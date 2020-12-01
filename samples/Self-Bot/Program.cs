fe2cc49f7d66e9974d0654511a4225e65ba44258
Shrek231 <49622239+shrek231@users.noreply.github.com>
2020-11-30 20:20:30 -0800

    Added sample

---
+++ samples/Self-Bot/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace Program
+{
+    class Program
+    {
+        private readonly DiscordSocketClient _client;
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public Program()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+        }
+
+        public async Task MainAsync()
+        {
+            await _client.LoginAsync(TokenType.User, "NjM5OTgzNjMzMjY3OTQ5NTY4.X8SKyA.wZAzkPXNNQP-QJbywA9kEL1EaM8");
+            await _client.StartAsync();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == "hi")
+                await message.Channel.SendMessageAsync("hi");
+        }
+    }
+}
